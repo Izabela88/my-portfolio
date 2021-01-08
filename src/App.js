10f35aed233402494fec2ba8d356c3c945d19c43
izak <izabkru88@gmail.com>
2021-01-08 16:31:52 +0000

    Created class components.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
@@ -5,18 +7,20 @@ import SinglePost from './components/SinglePost';
 import Post from './components/Post';
 import Project from './components/Project';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route component={Home} path='/' exact />
-        <Route component={About} path='/about' />
-        <Route component={SinglePost} path='/post/:slug' />
-        <Route component={Post} path='/post' />
-        <Route component={Project} path='/project' />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route component={Home} path='/' exact />
+          <Route component={About} path='/about' />
+          <Route component={SinglePost} path='/post/:slug' />
+          <Route component={Post} path='/post' />
+          <Route component={Project} path='/project' />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
