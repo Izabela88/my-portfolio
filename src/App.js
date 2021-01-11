ef1c838e683339aa95663cf606aacea2da2611f7
izak <izabkru88@gmail.com>
2021-01-11 14:40:52 +0000

    Created a Layout Component.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import About from './components/About/About';
 import SinglePost from './components/SinglePost/SinglePost';
 import Post from './components/Post/Post';
 import Project from './components/Project/Project';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route component={Home} path='/' exact />
-          <Route component={About} path='/about' />
-          <Route component={SinglePost} path='/post/:slug' />
-          <Route component={Post} path='/post' />
-          <Route component={Project} path='/project' />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        <Layout>
+          <BrowserRouter>
+            <Switch>
+              <Route component={Home} path='/' exact />
+              <Route component={About} path='/about' />
+              <Route component={SinglePost} path='/post/:slug' />
+              <Route component={Post} path='/post' />
+              <Route component={Project} path='/project' />
+            </Switch>
+          </BrowserRouter>
+        </Layout>
+      </div>
     );
   }
 }
