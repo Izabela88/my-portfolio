ea227144100165413aa89c3fe3f1f91bdcdf5fab
izak <izabkru88@gmail.com>
2021-01-20 14:23:04 +0000

    Styled Home page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,10 +10,13 @@ class Home extends Component {
         <img className={classes.MainImage} src={img3} alt='' />
         <img className={classes.MainImage1} src={img1} alt='' />
         <section className={classes.MainText}>
-          <h1>
-            Hi! I'm Izabela ! <br /> I'm so glad you're here !
-          </h1>
-          <p>Let's see what we've got here...</p>
+          <h1>Hi ! I'm Izabela ! I'm so glad you're here !</h1>
+          <p>
+            Welcome on my site! <br /> I'm glad you want to get to know me and
+            my projects. I hope you will enjoy watching my work. Have fun! :)
+            <br /> <br />
+            And now, let's see what we've got here...
+          </p>
         </section>
         <section className={classes.MainSideText}>
           <p>Welcome on my board!</p>
