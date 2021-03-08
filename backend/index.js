a883018d3cbdf6ff31d8821a8a0e133f95d596b7
izak <izabkru88@gmail.com>
2021-03-08 10:27:34 +0000

    Configuring Contact form with nodemailer.

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/', router);
+app.listen(5000, () => console.log('Server Running'));
+
+const contactEmail = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  auth: {
+    user: 'izabeladev88@gmail.com',
+    pass: '',
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Ready to Send');
+  }
+});
+
+router.post('/contact', (req, res) => {
+  const name = req.body.name;
+  const subject = req.body.subject;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: 'izabeladev88@gmail.com',
+    subject: 'Contact Form Message',
+    html: `<p>Name: ${name}</p> <p>Subject: ${subject}</p><p>Email: ${email}</p><p>Phone: ${phone}</p><p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json({ status: 'failed' });
+    } else {
+      res.json({ status: 'sent' });
+    }
+  });
+});
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,15 +2,60 @@ import React, { Component } from 'react';
 import classes from './Contact.module.css';
 import img from '../../assets/images/contact-img.jpeg';
 import Button from '../UI/Button/Button';
+import axios from 'axios';
 
 class Contact extends Component {
   state = {
     name: '',
     subject: '',
     email: '',
-    textarea: '',
+    phone: '',
+    message: '',
+    status: 'Submit',
   };
+
+  handleChange(event) {
+    const field = event.target.id;
+    if (field === 'name') {
+      this.setState({ name: event.target.value });
+    } else if (field === 'subject') {
+      this.setState({ subject: event.target.value });
+    } else if (field === 'email') {
+      this.setState({ email: event.target.value });
+    } else if (field === 'phone') {
+      this.setState({ phone: event.target.value });
+    } else if (field === 'message') {
+      this.setState({ message: event.target.value });
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ status: 'Sending' });
+    axios({
+      method: 'POST',
+      url: 'http://localhost:5000/contact',
+      data: this.state,
+    }).then((response) => {
+      if (response.data.status === 'sent') {
+        alert('Message Sent');
+        this.setState({
+          name: '',
+          subject: '',
+          email: '',
+          phone: '',
+          message: '',
+          status: 'Submit',
+        });
+      } else if (response.data.status === 'failed') {
+        alert('Message has not been sent.');
+      }
+    });
+  }
+
   render() {
+    let buttonText = this.state.status;
+
     return (
       <section className={classes.Contact} id='contact'>
         <div className={classes.Container}>
@@ -23,46 +68,57 @@ class Contact extends Component {
           <div className={classes.ContactText}>
             <p>
               Here is how you can reach me: <br /> izabkru88@gmail.com <br />
-              +44 7868197008
+              +44 7868197008 ada
             </p>
           </div>
           <div className={classes.ContactBox}>
             <div className={classes.ContactQuestion}>
               <p>Any questions?</p>
             </div>
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)} method='POST'>
               <div className={classes.TextFields}>
                 <input
                   type='text'
                   className={(classes.TextInput, classes.NameInput)}
                   placeholder='Name'
-                  name='name'
+                  id='name'
+                  value={this.state.name}
+                  onChange={this.handleChange.bind(this)}
+                  required
                 />
                 <input
                   type='text'
                   className={(classes.TextInput, classes.SubjectInput)}
                   placeholder='Subject'
-                  name='subject'
+                  id='subject'
+                  value={this.state.subject}
+                  onChange={this.handleChange.bind(this)}
                 />
                 <input
                   type='email'
                   className={(classes.TextInput, classes.EmailInput)}
                   placeholder='Email Address'
-                  name='email'
+                  id='email'
+                  value={this.state.email}
+                  onChange={this.handleChange.bind(this)}
                 />
                 <input
                   type='text'
                   className={(classes.TextInput, classes.PhoneInput)}
                   placeholder='Phone Number'
-                  name='phone'
+                  id='phone'
+                  value={this.state.phone}
+                  onChange={this.handleChange.bind(this)}
                 />
                 <textarea
                   className={(classes.TextInput, classes.MessageInput)}
                   placeholder='Enter Message'
-                  name='message'
+                  id='message'
+                  value={this.state.message}
+                  onChange={this.handleChange.bind(this)}
                 ></textarea>
               </div>
-              <Button>SUBMIT</Button>
+              <Button type='submit'>{buttonText}</Button>
             </form>
           </div>
         </div>
