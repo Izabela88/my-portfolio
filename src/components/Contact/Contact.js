09a46af8e616ded5912ed8d89289dc0c0f7adcb0
izak <izabkru88@gmail.com>
2021-03-12 17:36:16 +0000

    Used Formik package in Contact component.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,63 +1,22 @@
 import React, { Component } from 'react';
 import classes from './Contact.module.css';
 import img from '../../assets/images/contact-img.jpeg';
-import Button from '../UI/Button/Button';
 import axios from 'axios';
-import Input from '../UI/Input/Input';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import 'yup-phone';
+// import Button from '../UI/Button/Button';
+
+const Schema = Yup.object().shape({
+  name: Yup.string().required('Name required'),
+  subject: Yup.string().required('Subject required'),
+  email: Yup.string().email('Invalid email!').required('Email required'),
+  phone: Yup.string().phone().min(10).max(13).required('Phone required'),
+  message: Yup.string().max(600).required('Message required'),
+});
 
 class Contact extends Component {
-  state = {
-    name: '',
-    subject: '',
-    email: '',
-    phone: '',
-    message: '',
-    status: 'Submit',
-  };
-
-  handleChange(event) {
-    event.preventDefault();
-    const field = event.target.id;
-    if (field === 'name') {
-      this.setState({ name: event.target.value });
-    } else if (field === 'subject') {
-      this.setState({ subject: event.target.value });
-    } else if (field === 'email') {
-      this.setState({ email: event.target.value });
-    } else if (field === 'phone') {
-      this.setState({ phone: event.target.value });
-    } else if (field === 'message') {
-      this.setState({ message: event.target.value });
-    }
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    this.setState({ status: 'Sending' });
-    axios({
-      method: 'POST',
-      url: 'http://localhost:5000/contact',
-      data: this.state,
-    }).then((response) => {
-      if (response.data.status === 'sent') {
-        alert('Message Sent');
-        this.setState({
-          name: '',
-          subject: '',
-          email: '',
-          phone: '',
-          message: '',
-          status: 'Send',
-        });
-      } else if (response.data.status === 'failed') {
-        alert('Message has not been sent.');
-      }
-    });
-  }
-
   render() {
-    let buttonText = this.state.status;
-
     return (
       <section className={classes.Contact} id='contact'>
         <div className={classes.Container}>
@@ -73,58 +32,94 @@ class Contact extends Component {
               +44 7868197008
             </p>
           </div>
-          <div className={classes.ContactBox}>
-            <div className={classes.ContactQuestion}>
-              <p>Any questions?</p>
-            </div>
-            <form
-              className={classes.TextFields}
-              onSubmit={this.handleSubmit.bind(this)}
-              method='POST'
-            >
-              <Input
-                inputtype='input'
-                type='text'
-                placeholder='Name'
-                id='name'
-                value={this.state.name}
-                onChange={this.handleChange.bind(this)}
-                required
-              />
-              <Input
-                inputtype='input'
-                type='text'
-                placeholder='Subject'
-                id='subject'
-                value={this.state.subject}
-                onChange={this.handleChange.bind(this)}
-              />
-              <Input
-                inputtype='input'
-                type='email'
-                placeholder='Email Address'
-                id='email'
-                value={this.state.email}
-                onChange={this.handleChange.bind(this)}
-              />
-              <Input
-                inputtype='input'
-                type='text'
-                placeholder='Phone Number'
-                id='phone'
-                value={this.state.phone}
-                onChange={this.handleChange.bind(this)}
-              />
-              <Input
-                inputtype='textarea'
-                placeholder='Enter Message'
-                id='message'
-                value={this.state.message}
-                onChange={this.handleChange.bind(this)}
-              ></Input>
-            </form>
-          </div>
-          <Button type='submit'>{buttonText}</Button>
+
+          <Formik
+            validationSchema={Schema}
+            initialValues={{
+              name: '',
+              subject: '',
+              email: '',
+              phone: '',
+              message: '',
+            }}
+            onSubmit={(values) => {
+              this.setState({ status: 'Sending' });
+              axios({
+                method: 'POST',
+                url: 'http://localhost:5000/contact',
+                data: this.state,
+              }).then((response) => {
+                if (response.data.status === 'sent') {
+                  alert('Message Sent');
+                  this.setState({
+                    name: '',
+                    subject: '',
+                    email: '',
+                    phone: '',
+                    message: '',
+                  });
+                } else if (response.data.status === 'failed') {
+                  alert('Message has not been sent.');
+                }
+              });
+            }}
+          >
+            {({ errors, touched }) => (
+              <div className={classes.ContactBox}>
+                <div className={classes.ContactQuestion}>
+                  <p>Any questions?</p>
+                </div>
+                <Form>
+                  <Field
+                    className={classes.InputElement}
+                    name='name'
+                    placeholder='Name'
+                  />
+                  {errors.name && touched.name ? (
+                    <div className={classes.Error}>{errors.name}</div>
+                  ) : null}
+                  <Field
+                    className={classes.InputElement}
+                    name='subject'
+                    placeholder='Subject'
+                  />
+                  {errors.subject && touched.subject ? (
+                    <div className={classes.Error}>{errors.subject}</div>
+                  ) : null}
+                  <Field
+                    className={classes.InputElement}
+                    name='email'
+                    type='email'
+                    placeholder='Email Address'
+                  />
+                  {errors.email && touched.email ? (
+                    <div className={classes.Error}>{errors.email}</div>
+                  ) : null}
+                  <Field
+                    className={classes.InputElement}
+                    name='phone'
+                    type='phone'
+                    placeholder='Phone Number'
+                  />
+                  {errors.phone && touched.phone ? (
+                    <div className={classes.Error}>{errors.phone}</div>
+                  ) : null}
+                  <Field
+                    className={classes.InputElement}
+                    name='message'
+                    type='message'
+                    placeholder='Enter Message'
+                  />
+                  {errors.message && touched.message ? (
+                    <div className={classes.Error}>{errors.message}</div>
+                  ) : null}
+                  <button className={classes.ContactBtn} type='submit'>
+                    Submit
+                  </button>
+                </Form>
+              </div>
+            )}
+          </Formik>
         </div>
       </section>
     );
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import classes from './Projects.module.css';
 import sanityClient from '../../client.js';
-import img2 from '../../assets/images/img2.jpg';
-import img3 from '../../assets/images/img3.jpg';
-import img5 from '../../assets/images/img5.jpg';
 
 function Project() {
   const [projectData, setProjectData] = useState(null);
@@ -55,16 +52,6 @@ function Project() {
                     <h3 className={classes.TextTitle}>
                       <p>{project.title}</p>
                     </h3>
-                    {/* <p>
-                      <strong>Finished on:</strong>{' '}
-                      {new Date(project.date).toLocaleDateString()}
-                    </p> */}
-                    {/* <p>
-                      <strong>Company</strong>: {project.place}
-                    </p> */}
-                    {/* <p>
-                      <strong>Type:</strong> {project.projectType}
-                    </p> */}
                     <p>{project.description}</p>
                     <a
                       href={project.link}
@@ -77,38 +64,6 @@ function Project() {
                 </div>
               </div>
             ))}
-          <div className={classes.Item}>
-            <div className={classes.ItemImage}>
-              <img src={img2} alt='' />
-            </div>
-            <div className={classes.ItemText}>
-              <div className={classes.TextWrap}>
-                <h3 className={classes.TextTitle}>UI / UX</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className={classes.Item}>
-            <div className={classes.ItemImage}>
-              <img src={img3} alt='' />
-            </div>
-            <div className={classes.ItemText}>
-              <div className={classes.TextWrap}>
-                <h3 className={classes.TextTitle}> WEB DESIGN</h3>
-              </div>
-            </div>
-          </div>
-
-          <div className={classes.Item}>
-            <div className={classes.ItemImage}>
-              <img src={img5} alt='' />
-            </div>
-            <div className={classes.ItemText}>
-              <div className={classes.TextWrap}>
-                <h3 className={classes.TextTitle}>CREATIVITY</h3>
-              </div>
-            </div>
-          </div>
         </section>
       </section>
     </main>
