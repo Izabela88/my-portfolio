b3ccfabdb4af98bdb5edc46591bbf64b380d6c4a
izak <izabkru88@gmail.com>
2021-06-16 19:53:30 +0100

    form validation

--- src/components/Contact/Form/Form.js
+++ src/components/Contact/Form/Form.js
@@ -2,15 +2,7 @@ import React, { useState } from 'react';
 import classes from './Form.module.css';
 import Modal from '../../UI/Modal/Modal';
 import useModal from '../../UI/Modal/useModal';
-// import { Formik, Form, Field } from 'formik';
-// import * as Yup from 'yup';
-
-// const Schema = Yup.object().shape({
-//   name: Yup.string().max(40).required('Required'),
-//   subject: Yup.string().max(50).required('Required'),
-//   email: Yup.string().email('Invalid email!').required('Required'),
-//   message: Yup.string().max(600).required('Required'),
-// });
+import { useForm } from 'react-hook-form';
 
 export default function ContactForm() {
   const { isShowing, toggle } = useModal();
@@ -19,58 +11,90 @@ export default function ContactForm() {
   const [subject, setSubject] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [modal, setModal] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm();
 
   const handleClose = () => setShow(false);
 
-  const submitRequest = async (e) => {
-    e.preventDefault();
+  const submitRequestSuccess = async (data, e) => {
+    // console.log('submit request executed');
 
+    // console.log(data);
     const response = await fetch('http://localhost:5000/contact', {
       method: 'POST',
       headers: {
-        'Content-type': 'application/json',
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name, subject, email, message }),
+      body: JSON.stringify(data),
     });
-
-    const resData = await response.json();
-    if (resData.status === 'success') {
-      setShow(true);
-    } else if (resData.status === 'fail') {
-      alert('Message failed to send.');
+    if (response.status === 200) {
+      setModal(
+        <Modal
+          isShowing={isShowing}
+          hide={toggle}
+          show={show}
+          onHide={handleClose}
+        />
+      );
+    } else {
+      alert('Message not sent!');
     }
   };
 
   return (
-    <form onSubmit={submitRequest}>
-      <Modal
-        isShowing={isShowing}
-        hide={toggle}
-        show={show}
-        onHide={handleClose}
-      />
+    <form onSubmit={handleSubmit(submitRequestSuccess)}>
+      {modal}
       <div className={classes.ContactBox}>
         <div className={classes.ContactQuestion}>
           <p>Any questions?</p>
         </div>
 
         <input
+          {...register('name', {
+            validate: (value) => value.length > 2 && value.length < 20,
+            pattern: /^[A-Za-z]+$/i,
+            required: true,
+            isSubmitting: true,
+          })}
           className={classes.InputElement}
           name='name'
           placeholder='Name'
           onChange={(e) => setName(e.target.value)}
           value={name}
         />
+        {errors.name && (
+          <div className={classes.err}>
+            <p>Wrong Name!</p>
+          </div>
+        )}
 
         <input
+          {...register('subject', {
+            validate: (value) => value.length > 2 && value.length < 20,
+            pattern: /^[A-Za-z]+$/i,
+            required: true,
+            isSubmitting: true,
+          })}
           className={classes.InputElement}
           name='subject'
           placeholder='Subject'
           onChange={(e) => setSubject(e.target.value)}
           value={subject}
         />
+        {errors.subject && (
+          <div className={classes.err}>
+            <p>Wrong Subject!</p>
+          </div>
+        )}
 
         <input
+          {...register('email', {
+            required: true,
+          })}
           className={classes.InputElement}
           name='email'
           type='email'
@@ -78,8 +102,17 @@ export default function ContactForm() {
           onChange={(e) => setEmail(e.target.value)}
           value={email}
         />
+        {errors.message && (
+          <div className={classes.err}>
+            <p>Required!</p>
+          </div>
+        )}
 
         <textarea
+          {...register('message', {
+            required: true,
+            validate: (value) => value.length > 2 && value.length < 500,
+          })}
           className={`${classes.InputElement} ${classes.TextElement}`}
           name='message'
           type='message'
@@ -87,11 +120,16 @@ export default function ContactForm() {
           onChange={(e) => setMessage(e.target.value)}
           value={message}
         />
+        {errors.message && (
+          <div className={classes.err}>
+            <p>Required!</p>
+          </div>
+        )}
 
         <button
+          disabled={!isDirty}
           className={`${classes.ContactBtn} ${classes.Effect}`}
           type='submit'
-          onClick={toggle}
         >
           <span>Send Message</span>
         </button>
